1a17621b0bc280cd3f607c00b0a966764c8b931d
Elson Oliveira <elsonfojr@gmail.com>
2021-07-03 11:38:24 -0300

    RN init

---
+++ src/App.js
@@ -0,0 +1,29 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+const App = () => {
+  // const isDarkMode = useColorScheme() === 'dark';
+  /* return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+    </SafeAreaView>
+  ); */
+};
+
+export default App;
